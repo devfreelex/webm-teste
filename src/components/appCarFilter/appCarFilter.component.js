6b32c7825d47a71184c4d37bddbaa3a4c18e6f7e
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-10-02 09:29:07 -0300

    Atualizações - Agora o componente de lista está exibindo os dados corretamente. - No componente appCarFilter foram implementados os metodos que requisitam e definem as informações de preços, modelos e anos para a filtragem.

--- src/components/appCarFilter/appCarFilter.component.js
+++ src/components/appCarFilter/appCarFilter.component.js
@@ -34,16 +34,17 @@ export default () => {
 
     const hooks = ({ methods }) => ({
         beforeOnInit() {
+            methods.getVehicles()
             methods.getBrands()
             store.subscribe(dataStore => {
                 methods.getModels()
-
+                methods.getVersions()
             })
         }
     })
 
     const methods = () => {
-        let lastBrandId = null
+        let lastBrandId, lastModelId = null
 
         const getBrands = async () => {
             const httpService = http()
@@ -65,13 +66,73 @@ export default () => {
             const reqParams = { endPoint: 'model', query: 'MakeID', value: id }
             const data = await httpService.get(reqParams)
             const modelList = data.map(item => ({ id: item.ID, value: item.Name }))
-            store.update(dataStore => dataStore.model.list = modelList)
+            store.update(dataStore => dataStore.model.list = modelList )
 
         }
 
+        const getVersions = async () => {
+            const dataStore = store.get()
+            const { id } = dataStore.model.selected
+            const hasInvalidQuery = !id || lastModelId === id
+
+            if (hasInvalidQuery) return
+
+            lastModelId = id
+            const httpService = http()
+            const reqParams = { endPoint: 'version', query: 'ModelID', value: id }
+            const data = await httpService.get(reqParams)
+            console.log(data)
+            const version = data.map(item => ({ id: item.ID, value: item.Name }))
+            store.update(dataStore => dataStore.version.list = version)
+        }        
+
+        const getVehicles = async () => {
+            const httpService = http()
+            const reqParams = { endPoint: 'vehicles', query: 'Page', value: 1 }
+            const vehicleList = await httpService.get(reqParams)
+            store.update(dataStore => {
+                dataStore.vehicle.list = vehicleList
+                setYears(dataStore)
+                setPrices(dataStore)
+            })            
+        }
+
+        const getUniqValues = (data) => {
+            return data.filter(function (item, pos, ary) {
+                return !pos || item.value != ary[pos - 1]['value'];
+            });            
+        }
+
+        const sortValues = (data) => {
+            return data.sort((a, b) => a.value - b.value)
+        }
+
+        const setYears = (dataStore) => {
+            const vehicles = store.get().vehicle.list
+            const data = vehicles.map( (vehicle, index) => ({id: index + 1, value: vehicle.YearFab}))
+            const sortedYears = sortValues(data)
+            const uniqYears = getUniqValues(sortedYears)
+
+            dataStore.year.list = uniqYears
+        }
+
+        const setPrices = (dataStore) => {
+            const vehicles = store.get().vehicle.list
+            const data = vehicles.map((vehicle, index) => ({ id: index + 1, value: parseFloat(vehicle.Price) }))
+            const sortedPrices = sortValues(data)
+            const uniquePrices = getUniqValues(sortedPrices)
+
+            dataStore.priceRange.list = uniquePrices
+        }
+
+
         return {
             getBrands,
-            getModels
+            getModels,
+            getVersions,
+            getVehicles,
+            setYears,
+            setPrices
         }
     }
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
 import { storeFactory } from 'r9x_js'
 
 const store = storeFactory({
-
+    vehicle:{
+        list:[]
+    },
     place: { 
         isVisible: false,
-        list: [
-            { value: 'val 1', id: 1 },
-            { value: 'val 2', id: 2 },
-            { value: 'val 3', id: 3 },
-            { value: 'val 4', id: 4 },
-            { value: 'val 5', id: 5 },
-        ]          
+        list: []          
     },
     range: { 
         isVisible: false,
@@ -36,24 +32,22 @@ const store = storeFactory({
         selected: { id: null, value: '' }         
     },
     year: { 
+        value:'',
         isVisible: false,
-        list: [
-            { value: 'val 1', id: 1 },
-            { value: 'val 2', id: 2 },
-            { value: 'val 3', id: 3 },
-            { value: 'val 4', id: 4 },
-            { value: 'val 5', id: 5 },
-        ]          
+        list: [],
+        selected: { id: null, value: '' }          
+    },
+    price: { 
+        value:'',
+        isVisible: false,
+        list: [],
+        selected: { id: null, value: '' }          
     },
     priceRange: { 
+        value: '',
         isVisible: false,
-        list: [
-            { value: 'val 1', id: 1 },
-            { value: 'val 2', id: 2 },
-            { value: 'val 3', id: 3 },
-            { value: 'val 4', id: 4 },
-            { value: 'val 5', id: 5 },
-        ]          
+        list: [],
+        selected: { id: null, value: '' }              
     },
     version: { 
         isVisible: false,
