3b29ccf61b4fd463e9d7af30bba0b643bc97f0ef
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-10-06 15:22:21 -0300

    BugFix - Ao clicar no checkbox agora simplesmente o estado é definido para true, isso remove o bug provocado pela definição de tipo.

--- src/components/appCheck/appCheck.component.js
+++ src/components/appCheck/appCheck.component.js
@@ -10,19 +10,11 @@ export default () => {
         checked: false,
     }
 
-    const hooks = ({ methods }) => ({
-        beforeOnInit () {
-            // methods.updateState()
-        }
-    })
-
     const events = ({on, query, methods}) => ({
         onCheckButton () {
             const checkButton = query('.btn-check')
             on('click', [checkButton], ({target}) => {
                 methods.toggleCheck()
-                methods.setType()
-                methods.logState()
             })
         }
     })
@@ -31,19 +23,6 @@ export default () => {
         toggleCheck () {
             state.set({checked: !state.checked})
                 
-        },
-        setType () {
-            const { type } = props.get().object
-            store.update( dataStore => {
-                type === 'new' ? 
-                    dataStore.vehicleType.new = !dataStore.vehicleType.new : 
-                    dataStore.vehicleType.worn = !dataStore.vehicleType.worn 
-            })
-
-        },
-
-        logState () { 
-            console.log(store.get().vehicleType)
         }
     })
 
@@ -54,6 +33,5 @@ export default () => {
         styles,
         events,
         methods,
-        hooks
     }
 }
